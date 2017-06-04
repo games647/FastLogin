7db8c78975679f0e78f6d3903c40ed1dc4d114b8
games647 <games647@users.noreply.github.com>
2017-06-04 15:14:59 +0200

    Drop support for old authme API

--- bukkit/src/main/java/com/github/games647/fastlogin/bukkit/hooks/AuthMeHook.java
+++ bukkit/src/main/java/com/github/games647/fastlogin/bukkit/hooks/AuthMeHook.java
@@ -2,10 +2,7 @@ package com.github.games647.fastlogin.bukkit.hooks;
 
 import com.github.games647.fastlogin.core.hooks.AuthPlugin;
 
-import fr.xephi.authme.api.API;
 import fr.xephi.authme.api.NewAPI;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.bukkit.entity.Player;
 
@@ -20,58 +17,27 @@ import org.bukkit.entity.Player;
  */
 public class AuthMeHook implements AuthPlugin<Player> {
 
-    private final boolean isNewAPIAvailable;
-
-    public AuthMeHook() {
-        boolean apiAvailable = false;
-        try {
-            Class.forName("fr.xephi.authme.api.NewAPI");
-            apiAvailable = true;
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(AuthMeHook.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        this.isNewAPIAvailable = apiAvailable;
-    }
-
     @Override
-    @SuppressWarnings("deprecation")
     public boolean forceLogin(Player player) {
         //skips registration and login
-        if (isNewAPIAvailable) {
-            if (NewAPI.getInstance().isAuthenticated(player)) {
-                return false;
-            } else {
-                NewAPI.getInstance().forceLogin(player);
-            }
-        } else if (!API.isAuthenticated(player)) {
-            API.forceLogin(player);
+        if (NewAPI.getInstance().isAuthenticated(player)) {
+            return false;
+        } else {
+            NewAPI.getInstance().forceLogin(player);
         }
 
         return true;
     }
 
     @Override
-    @SuppressWarnings("deprecation")
     public boolean isRegistered(String playerName) throws Exception {
-        if (isNewAPIAvailable) {
-            return NewAPI.getInstance().isRegistered(playerName);
-        } else {
-            return API.isRegistered(playerName);
-        }
+        return NewAPI.getInstance().isRegistered(playerName);
     }
 
     @Override
-    @SuppressWarnings("deprecation")
     public boolean forceRegister(Player player, String password) {
-        if (isNewAPIAvailable) {
-            //this automatically registers the player too
-            NewAPI.getInstance().forceRegister(player, password);
-        } else {
-            API.registerPlayer(player.getName(), password);
-            forceLogin(player);
-        }
-
+        //this automatically registers the player too
+        NewAPI.getInstance().forceRegister(player, password);
         return true;
     }
 }
